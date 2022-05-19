8a4bacd7adf3c214137de05291b126484c7c92d8
Guillaume Maze <gmaze@ifremer.fr>
2022-05-19 12:49:47 +0200

    Update load_classif_save.py

--- load_classif_save.py
+++ load_classif_save.py
@@ -1,11 +1,9 @@
-import pandas as pd
-import xarray as xr
-xr.set_options(display_style="html", display_expand_attrs=False)
+import os
+# import pandas as pd
+# import xarray as xr
+# xr.set_options(display_style="html", display_expand_attrs=False)
 
-# from argopy.stores.argo_index_pd import indexstore_pandas as indexstore
-
-import sys, os
-# sys.path.insert(0, os.sep.join([os.getcwd(), '..']))
+from argopy.stores.argo_index_pd import indexstore_pandas as indexstore
 from pcmbc.utilities import load_aviso_nrt, load_aviso_mdt
 
 if __name__ == '__main__':
@@ -19,13 +17,15 @@ if __name__ == '__main__':
     if not WEKEO_USERNAME:
         raise ValueError("No WEKEO_USERNAME in environment ! ")
 
-    # index_file = "https://raw.githubusercontent.com/euroargodev/boundary_currents_pcm/main/data/BCindex_%s.txt" % (BC.replace(" ", "_").replace(".", ""))
-    # print("index_file", index_file)
-    # idx = indexstore(host=os.path.split(index_file)[0], index_file=os.path.split(index_file)[1])
-    # index = idx.to_dataframe()
+    index_file = "https://raw.githubusercontent.com/euroargodev/boundary_currents_pcm/main/data/BCindex_%s.txt" % (BC.replace(" ", "_").replace(".", ""))
+    print("index_file", index_file)
+    idx = indexstore(host=os.path.split(index_file)[0], index_file=os.path.split(index_file)[1])
+    index = idx.to_dataframe()
 
-    # aviso = load_aviso_nrt(box, index['date'].max(), WEKEO_USERNAME, WEKEO_PASSWORD, vname='sla')
-    aviso = load_aviso_nrt(box, pd.to_datetime('now', utc=True), WEKEO_USERNAME, WEKEO_PASSWORD, vname='sla')
+    aviso = load_aviso_nrt(box, index['date'].max(), WEKEO_USERNAME, WEKEO_PASSWORD, vname='sla')
+    # aviso = load_aviso_nrt(box, pd.to_datetime('now', utc=True), WEKEO_USERNAME, WEKEO_PASSWORD, vname='sla')
+
+    aviso_clim = load_aviso_mdt(box, index['date'].max(), WEKEO_USERNAME, WEKEO_PASSWORD, vname='mdt')
+
     print(aviso)
-
-    # aviso_clim = load_aviso_mdt(box, index['date'].max(), WEKEO_USERNAME, WEKEO_PASSWORD, vname='mdt')
+    print(aviso_clim)
